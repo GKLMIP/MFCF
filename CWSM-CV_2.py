097f4e3e24af569d24557a10cf9424a6bb3a1914
GKLMIP <82089160+GKLMIP@users.noreply.github.com>
2022-03-28 12:27:11 +0800

    Add files via upload

---
+++ CWSM-CV_2.py
@@ -0,0 +1,75 @@
+import numpy as np
+from gensim.models import word2vec
+import pickle
+import gensim
+import csv
+
+model_loc = 'model/charmodel_LD_2'
+with open('data/全部距离2候选词列表','rb') as f:
+    candidates = pickle.load(f)
+model = gensim.models.Word2Vec.load(model_loc)
+
+whole = 0
+right = 0
+with open('data/Ind_2_70%_dataset.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        wrong_wo = row[1]
+        word_vector = []
+        vec = []
+        if wrong_wo not in candidates:
+            continue
+        if len(candidates[wrong_wo]) != 0:
+            wor_list = candidates[wrong_wo]
+        else:
+            continue
+
+        whole += 1
+        for wor in wor_list:
+            final_vec = model[wor[0]]
+            i = 0
+            for cha in wor:
+                if i == 0:
+                    i += 1
+                    continue
+                final_vec = final_vec + model[cha]
+            final_vec = final_vec / len(wor)
+            vec.append(final_vec)
+            word_vector.append(wor)
+
+        vec = np.array(vec)
+        wrong_word_vec = model[wrong_wo[0]]
+        i = 0
+        for cha in wrong_wo:
+            if i == 0:
+                i += 1
+                continue
+            if cha not in model:
+                continue
+            wrong_word_vec = wrong_word_vec + model[cha]
+        wrong_word_vec = wrong_word_vec / len(wrong_wo)
+
+        rank = {}
+        rank_cos = []
+        for i in range(len(word_vector)):
+            dot = np.dot(vec[i], wrong_word_vec)
+            norma = np.linalg.norm(vec[i])
+            normb = np.linalg.norm(wrong_word_vec)
+            cos = dot / (norma * normb)
+            rank[cos] = word_vector[i]
+            rank_cos.append(cos)
+        rank_cos = sorted(rank_cos, reverse=True)
+
+        result = []
+        for nu in rank_cos:
+            result.append(rank[nu])
+        if row[2] in result[:1]:
+            right += 1
+
+print(right)
+print(whole)
+num_rate = right/whole
+print('正确率：'+str(num_rate))
+
+
+
